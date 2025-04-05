65a0e55bae9f5c71b213246dba49f4ad6acd7b7e
snoopyylion <tifase_oluwaseyi@yahoo.com>
2025-04-05 11:02:59 +0100

    updated

--- components/Message.jsx
+++ components/Message.jsx
@@ -3,12 +3,21 @@ import Image from 'next/image'
 import React, { useEffect } from 'react'
 import Markdown from 'react-markdown'
 import Prism from 'prismjs'
+import toast from 'react-hot-toast'
 
 const Message = ({ role, content}) => {
+
+    const cleanContent = content.replace(/\\boxed{([^}]+)}/g, '$1');
+
+
 
     useEffect(()=> {
         Prism.highlightAll();
     },[content])
+    const copyMessage = () => {
+        navigator.clipboard.writeText(content);
+        toast.success('Message copied to clipboard');
+    }
   return (
     <div className='flex flex-col items-center w-full max-w-3xl text-sm'>
         <div className={`flex flex-col w-full mb-8 ${ role === 'user' && 'items-end' }`}>
@@ -18,12 +27,12 @@ const Message = ({ role, content}) => {
                         {
                             role === 'user' ? (
                                 <>
-                                <Image src={assets.copy_icon} alt='' className='w-4 cursor-pointer'/>
+                                <Image onClick={copyMessage} src={assets.copy_icon} alt='' className='w-4 cursor-pointer'/>
                                 <Image src={assets.pencil_icon} alt='' className='w-4.5 cursor-pointer'/>
                                 </>
                             ):(
                                 <>
-                                <Image src={assets.copy_icon} alt='' className='w-4.5 cursor-pointer'/>
+                                <Image onClick={copyMessage} src={assets.copy_icon} alt='' className='w-4.5 cursor-pointer'/>
                                 <Image src={assets.regenerate_icon} alt='' className='w-4 cursor-pointer'/>
                                 <Image src={assets.like_icon} alt='' className='w-4 cursor-pointer'/>
                                 <Image src={assets.dislike_icon} alt='' className='w-4 cursor-pointer'/>
@@ -44,7 +53,7 @@ const Message = ({ role, content}) => {
                         <>
                         <Image src={assets.logo_icon} alt='' className='h-9 w-9 p-1 border border-white/15 rounded-full'/>
                         <div className='space-y-4 w-full overflow-scroll'>
-                            <Markdown>{content}</Markdown>
+                            <Markdown>{cleanContent}</Markdown>
                         </div>
                         </>
                      )
